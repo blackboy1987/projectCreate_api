12dcb053b036345c5c0ff140c4cd8b5c93385758
xiali <1169794338@qq.com>
2023-11-03 10:26:13 +0800

    init

---
+++ src/main/java/com/bootx/ProjectCreateApplication.java
@@ -0,0 +1,13 @@
+package com.bootx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectCreateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectCreateApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/bootx/audit/AuditorProvider.java
@@ -0,0 +1,13 @@
+
+package com.bootx.audit;
+
+public interface AuditorProvider<T> {
+
+	/**
+	 * 获取当前审计者
+	 * 
+	 * @return 当前审计者
+	 */
+	T getCurrentAuditor();
+
+}
